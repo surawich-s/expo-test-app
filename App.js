e1e7263f15c6cce0c9987de23b26510e0dce755a
Surawich Sittisarn <surawich.sit@gmail.com>
2023-01-27 21:37:34 +0700

    add: emojilist and sticker

--- App.js
+++ App.js
@@ -7,12 +7,16 @@ import ImageViewer from "./components/ImageViewer";
 import CircleButton from "./components/CircleButton";
 import IconButton from "./components/IconButton";
 import EmojiPicker from "./components/EmojiPicker";
+import EmojiList from "./components/EmojiList";
+import EmojiSticker from "./components/EmojiSticker";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
     const [selectedImage, setSelectedImage] = useState(null);
     const [showAppOptions, setShowAppOptions] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [pickedEmoji, setPickedEmoji] = useState(null);
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             allowsEditing: true,
@@ -50,6 +54,9 @@ export default function App() {
                     placeholderImageSource={PlaceholderImage}
                     selectedImage={selectedImage}
                 />
+                {pickedEmoji !== null ? (
+                    <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+                ) : null}
             </View>
             {showAppOptions ? (
                 <View style={styles.optionsContainer}>
@@ -81,7 +88,10 @@ export default function App() {
                 </View>
             )}
             <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-                {/* A list of emoji component will go here */}
+                <EmojiList
+                    onSelect={setPickedEmoji}
+                    onCloseModal={onModalClose}
+                />
             </EmojiPicker>
             <StatusBar style="auto" />
         </View>
