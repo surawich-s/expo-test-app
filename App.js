a3dec6d15e4d9ebd5462c1b6027f72891809bf3f
Surawich Sittisarn <surawich.sit@gmail.com>
2023-01-30 18:58:02 +0700

    add: pan and tap emojisticker

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as ImagePicker from "expo-image-picker";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Button from "./components/Button";
 import ImageViewer from "./components/ImageViewer";
 import CircleButton from "./components/CircleButton";
@@ -48,7 +49,7 @@ export default function App() {
     };
 
     return (
-        <View style={styles.container}>
+        <GestureHandlerRootView style={styles.container}>
             <View style={styles.imageContainer}>
                 <ImageViewer
                     placeholderImageSource={PlaceholderImage}
@@ -94,7 +95,7 @@ export default function App() {
                 />
             </EmojiPicker>
             <StatusBar style="auto" />
-        </View>
+        </GestureHandlerRootView>
     );
 }
 
--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,13 +1,77 @@
 import { View, Image } from "react-native";
+import {
+    TapGestureHandler,
+    PanGestureHandler,
+} from "react-native-gesture-handler";
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    useAnimatedGestureHandler,
+    withSpring,
+} from "react-native-reanimated";
+
+const AnimatedView = Animated.createAnimatedComponent(View);
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+
+    const scaleImage = useSharedValue(imageSize);
+
+    const onDoubleTap = useAnimatedGestureHandler({
+        onActive: () => {
+            if (scaleImage.value) {
+                scaleImage.value = scaleImage.value * 2;
+            }
+        },
+    });
+
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value),
+        };
+    });
+
+    const onDrag = useAnimatedGestureHandler({
+        onStart: (event, context) => {
+            context.translateX = translateX.value;
+            context.translateY = translateY.value;
+        },
+        onActive: (event, context) => {
+            translateX.value = event.translationX + context.translateX;
+            translateY.value = event.translationY + context.translateY;
+        },
+    });
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: translateX.value,
+                },
+                {
+                    translateY: translateY.value,
+                },
+            ],
+        };
+    });
+
     return (
-        <View style={{ top: -350 }}>
-            <Image
-                source={stickerSource}
-                resizeMode="contain"
-                style={{ width: imageSize, height: imageSize }}
-            />
-        </View>
+        <PanGestureHandler onGestureEvent={onDrag}>
+            <AnimatedView style={{ top: -350 }}>
+                <TapGestureHandler
+                    onGestureEvent={onDoubleTap}
+                    numberOfTaps={2}
+                >
+                    <AnimatedImage
+                        source={stickerSource}
+                        resizeMode="contain"
+                        style={{ width: imageSize, height: imageSize }}
+                    />
+                </TapGestureHandler>
+            </AnimatedView>
+        </PanGestureHandler>
     );
 }
