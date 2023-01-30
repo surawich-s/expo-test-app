a5d365dd242995379f0bc64043fa9261d34471dc
Surawich Sittisarn <surawich.sit@gmail.com>
2023-01-30 19:15:59 +0700

    add: light status

--- App.js
+++ App.js
@@ -118,7 +118,7 @@ export default function App() {
                     onCloseModal={onModalClose}
                 />
             </EmojiPicker>
-            <StatusBar style="auto" />
+            <StatusBar style="light" />
         </GestureHandlerRootView>
     );
 }
